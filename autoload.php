40b47477fa360f314098c174a682941bc36081e0
Mirco Cuccadu <mirco.cuccadu@spindox.it>
2024-05-22 16:11:35 +0200

    feat: add support for other modules rather than divi native

--- autoload.php
+++ autoload.php
@@ -5,15 +5,36 @@ class skh_DiviChild_Autoload
     private static ?skh_DiviChild_Autoload $instance = null;
     private static RecursiveIteratorIterator $iterator;
     private static array $classlist = [];
-    private const ASSETS_URL = DIVI_CHILD_MODULES_URL."/assets";
+    private const ASSETS_URL = DIVI_CHILD_MODULES_URL . "/assets";
+    private static array $divi_child_extra_modules_paths;
 
     private function __construct()
     {
         $dir = new RecursiveDirectoryIterator(DIVI_CHILD_MODULES_PATH);
         self::$iterator = new RecursiveIteratorIterator($dir);
+        self::$divi_child_extra_modules_paths = [
+            [
+                "iterator" => self::createIterator(DIVI_CHILD_BP . "/includes/supreme-modules-pro-for-divi"),
+                "classNameParser" => function ($fname)
+                {
+                    if (strpos($fname, '.php') !== false)
+                        $fname = 'skh_DSM_' . implode('_', preg_split('/(?=[A-Z])/', basename($fname, '.php'), -1, PREG_SPLIT_NO_EMPTY));
+                    return $fname;
+                }
+            ]
+        ];
         add_action('et_builder_ready', [__CLASS__, 'autoload_divi_child_modules']);
         add_filter('et_module_classes', [__CLASS__, 'divi_custom_module_class']);
+        add_action('et_builder_ready', [__CLASS__, 'autoload_divi_child_extra_modules']);
+        add_filter('et_module_classes', [__CLASS__, 'divi_extra_module_class']);
         self::register_divi_child_modules_js();
+    }
+
+    private static function createIterator($path)
+    {
+        if(is_dir($path))
+            return new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path));
+        return null;
     }
 
     public static function getInstance()
@@ -27,14 +48,14 @@ class skh_DiviChild_Autoload
         return self::$instance;
     }
 
-    private static function process_divi_child_modules(object $cb)
+    private static function process_divi_child_modules(RecursiveIteratorIterator $iterator, object $cb, object $classNameParser)
     {
-        foreach (self::$iterator as $file)
+        foreach ($iterator as $file)
         {
             $fname = $file->getFilename();
             $fpath = $file->getPath();
             $fileFolders = explode(DIRECTORY_SEPARATOR, $fpath);
-            $className = "skh_ET_Builder_Module_" . str_replace('.php', '', $fname);
+            $className = $classNameParser($fname);
             if (
                 preg_match('%\.php$%', $fname)
                 && !in_array('templates', $fileFolders)
@@ -49,23 +70,49 @@ class skh_DiviChild_Autoload
 
     static function autoload_divi_child_modules()
     {
-        self::process_divi_child_modules(function ($file, $fname, $fpath, $fileFolders, $className)
-        {
-            $instance = new $className;
-            add_shortcode($instance::$shortcode, array($instance, '_shortcode_callback'));
-        });
+        self::process_divi_child_modules(
+            self::$iterator,
+            function ($file, $fname, $fpath, $fileFolders, $className)
+            {
+                $instance = new $className;
+                add_shortcode($instance::$shortcode, array($instance, '_shortcode_callback'));
+            },
+            function ($fname)
+            {
+                return "skh_ET_Builder_Module_" . str_replace('.php', '', $fname);
+            }
+        );
     }
 
     static function divi_custom_module_class($classlist)
     {
         self::process_divi_child_modules(
+            self::$iterator,
             function ($file, $fname, $fpath, $fileFolders, $className) use ($classlist)
             {
                 include_once($file->getPathname());
                 self::$classlist[$className::$shortcode] = array('classname' => $className);
+            },
+            function ($fname)
+            {
+                return "skh_ET_Builder_Module_" . str_replace('.php', '', $fname);
             }
         );
         return  array_merge($classlist, self::$classlist);
+    }
+
+    static function divi_extra_module_class($classlist)
+    {
+        foreach (self::$divi_child_extra_modules_paths as $extraModuleInfo)
+        {
+            if(!$extraModuleInfo['iterator']) continue;
+            self::process_divi_child_modules($extraModuleInfo['iterator'], function ($file, $fname, $fpath, $fileFolders, $className)
+            {
+                include_once($file->getPathname());
+                self::$classlist[$className::$shortcode] = array('classname' => $className);
+            }, $extraModuleInfo['classNameParser']);
+        }
+        return $classlist;
     }
 
     private static function register_divi_child_modules_js()
@@ -73,17 +120,34 @@ class skh_DiviChild_Autoload
         foreach (self::$iterator as $file)
         {
             $fname = $file->getFilename();
+            $fpath = $file->getPath();
+            $fileFolders = explode(DIRECTORY_SEPARATOR, $fpath);
             if (
                 preg_match('%\.js$%', $fname)
+                && in_array('assets', $fileFolders)
             )
             {
                 $fpath = $file->getPath();
                 $handle = str_replace('.js', '-js', $fname);
                 $ver = defined('WP_DEBUG') && WP_DEBUG ? time() : DIVI_CHILD_VERSION;
-                wp_register_script("skh-divi-child-$handle", self::ASSETS_URL."/js/$fname", ['jquery'], $ver);
+                wp_register_script("skh-divi-child-$handle", self::ASSETS_URL . "/js/$fname", ['jquery'], $ver);
             }
         }
     }
+
+    static function autoload_divi_child_extra_modules()
+    {
+        foreach (self::$divi_child_extra_modules_paths as $extraModuleInfo)
+        {
+            if(!$extraModuleInfo['iterator']) continue;
+            self::process_divi_child_modules($extraModuleInfo['iterator'], function ($file, $fname, $fpath, $fileFolders, $className)
+            {
+                $instance = new $className;
+                add_shortcode($instance::$shortcode, array($instance, '_shortcode_callback'));
+            }, $extraModuleInfo['classNameParser']);
+        }
+    }
+
 }
 
 
