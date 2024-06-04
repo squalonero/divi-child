c6ab395381775544448191d64776925a1217cd8c
Mirco Cuccadu <mirco.cuccadu@spindox.it>
2024-06-04 16:55:55 +0200

    feat: add skip to content and skip to language

---
+++ includes/Header.php
@@ -0,0 +1,19 @@
+<?php
+
+
+class skh_Header
+{
+    static function init()
+    {
+        add_action('wp_body_open', [__CLASS__, 'skip_to_content']);
+    }
+
+    static function skip_to_content()
+    {
+        ob_start();
+        include DIVI_CHILD_TEMPLATES_PATH . '/skip_to_content.php';
+        echo ob_get_clean();
+    }
+}
+
+skh_Header::init();
---
+++ templates/skip_to_content.php
@@ -0,0 +1,6 @@
+<div id="skip-to-content">
+    <a href="#main-content"><?= __('Skip to content', 'stackhouse') ?></a>
+</div>
+<div id="skip-to-language">
+    <a href="#main-content"><?= __('Skip to select language', 'stackhouse') ?></a>
+</div>
