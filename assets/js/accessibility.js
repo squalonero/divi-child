4cc117cc024591ba17891f5353e871fdbdaf1529
Mirco Cuccadu <mirco.cuccadu@spindox.it>
2024-06-06 17:10:38 +0200

    fix: remove user-scalable from meta viewport

--- assets/js/accessibility.js
+++ assets/js/accessibility.js
@@ -3,8 +3,16 @@
     // $('*').on('focus', (e)=> console.log('focused', e.target)); //debug
     $("#skip-to-content>a").on("keydown", function (e) {
       var code = e.keyCode || e.which;
-      if (code == 13) //Enter
+      if (code == 13)
+        //Enter
         $("#main-content a, #main-content button").first().focus();
     });
+
+    /**
+     * @WORKAROUND
+     * remove bad accessibility attributes from Divi head tags
+     * width=device-width, initial-scale=1.0, maximum-scale=1.0
+     */
+    $('meta[name="viewport"]').attr({content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0'});
   });
 })(jQuery);
