42dcef10d715095f01680c1454142a12dc528b67
marcomadeddu <marcomade99@gmail.com>
2024-06-07 14:12:40 +0200

    fix: maximum-scale

--- includes/DiviOverrides.php
+++ includes/DiviOverrides.php
@@ -11,6 +11,6 @@ class skh_DiviOverrides
 
     static function et_add_viewport_meta()
     {
-        echo '<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />';
+        echo '<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=2.0" />';
     }
 }
