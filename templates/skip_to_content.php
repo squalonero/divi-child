738c04d92d5205dcb55688c3724e89b4ec19bdb6
Mirco Cuccadu <mirco.cuccadu@spindox.it>
2024-06-17 14:47:04 +0200

    fix: accessibility skip-tos css adjust when admin bar visible

--- templates/skip_to_content.php
+++ templates/skip_to_content.php
@@ -1,6 +1,6 @@
-<div id="skip-to-content">
+<div id="skip-to-content" class="<?php echo $classes; ?>">
     <a href="#main-content"><?= __('Skip to content', 'stackhouse') ?></a>
 </div>
-<div id="skip-to-language">
+<div id="skip-to-language" class="<?php echo $classes; ?>">
     <a href="#main-content"><?= __('Skip to select language', 'stackhouse') ?></a>
 </div>
