bcb9e60b882ef76b0e692ebc4549a2bb06e02621
Mirco Cuccadu <mirco.cuccadu@spindox.it>
2024-07-11 17:02:43 +0200

    feat: adjust go to main content selector

--- assets/js/accessibility.js
+++ assets/js/accessibility.js
@@ -5,7 +5,10 @@
       var code = e.keyCode || e.which;
       if (code == 13)
         //Enter
-        $("#main-content a, #main-content button").first().focus();
+        $("#main-content a:visible, #main-content button:visible")
+        .not('.dsm_breadcrumbs_wrap a')
+        .not('.dsm_breadcrumbs_wrap button')
+        .first().focus();
     });
   });
 })(jQuery);
