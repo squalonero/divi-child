98ac6e102cc0998cc8cdcd56ed01aa86c5a78487
MarcoMadeddu <marcomade99@gmail.com>
2024-07-22 18:17:36 +0200

    aligned to collaudo

--- assets/js/accessibility.js
+++ assets/js/accessibility.js
@@ -12,12 +12,17 @@
     });
     $("#skip-to-language>a").on("keydown", function (e) {
       var code = e.keyCode || e.which;
-      if (code == 13)
-        //Enter
-        $("#mega-menu-item-wpml-ls-9-it a:visible")
-        .first().focus();
+      var link = $("#mega-menu-item-wpml-ls-9-it  a:visible");
+      if (code == 13){
+        if(link.length < 2){
+          $("#mega-menu-item-wpml-ls-9-en  a:visible").first().focus();
+        }else{
+          $("#mega-menu-item-wpml-ls-9-it a:visible").first().focus();
+        }
+      }
     });
-
+      
+        
     // Accessibilty on social icon
     $("#skh_social_button > li > a").attr("role", "button");
   });
